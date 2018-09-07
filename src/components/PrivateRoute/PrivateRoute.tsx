969ba7a28a2573bd5e565fe4e6c6baae0122db7d
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-09-07 08:29:01 +0200

    Implemented PrivateRoute HOC

---
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router';
+
+interface IPrivateRouteProps {
+  component: any;
+  auth: any;
+  exact: boolean;
+  path: string;
+}
+
+const PrivateRoute = ({ component: Component, auth, ...rest }: IPrivateRouteProps) => (
+  <Route
+    {...rest}
+    render={props => (auth.isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />)}
+  />
+);
+
+const mapStateToProps = (state: any) => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
