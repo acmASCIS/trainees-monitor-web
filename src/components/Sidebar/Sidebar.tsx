1058957a7d62302aaba0e8f6f11cdb8d4d263911
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-09-14 10:38:29 +0200

    Implemented logout button

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import logo from '../../assets/logo.png';
 import './Sidebar.css';
+import { logoutUser } from '../../actions/authActions';
 
-const Sidebar = () => {
+interface ISidebarProps {
+  logoutUser: () => void;
+}
+
+const Sidebar = (props: ISidebarProps) => {
   return (
     <div className="side-menu height-100 bg-primary">
       <Link to="" className="logo text-center my-2">
@@ -30,8 +36,14 @@ const Sidebar = () => {
           </Link>
         </li>
       </ul>
+      <button type="button" className="btn btn-outline-light m-5" onClick={props.logoutUser}>
+        Logout
+      </button>
     </div>
   );
 };
 
-export default Sidebar;
+export default connect(
+  null,
+  { logoutUser }
+)(Sidebar);
